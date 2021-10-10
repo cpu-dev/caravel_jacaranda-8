016bdca41c61ef949b28f90020f2582cca0fc680
Yuki Azuma <yuhki.yasuda@gmail.com>
2021-10-10 20:26:15 +0900

    add jacaranda-8 toolchain

---
+++ verilog/dv/jacaranda_test/uart.s
@@ -0,0 +1,26 @@
+ldih 0
+ldil 1
+mov r0, r3
+ldih f
+ldil f
+st r0, r3
+ldih 4
+ldil 1
+mov r0, r3
+ldih f
+ldil d
+mov r1, r3
+ldih f
+ldil e
+ld r2, r3
+ldih 0
+ldil 1
+and r2, r3
+cmp r2, r3
+ldih 0
+ldil c
+je r3
+st r0, r1
+ldih 0
+ldil 6
+jmp r3
