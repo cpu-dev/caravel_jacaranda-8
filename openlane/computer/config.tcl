525e546b81666a6784809107a65973ad14629347
Yuki Azuma <yuhki.yasuda@gmail.com>
2021-10-21 15:30:04 +0900

    いい感じ

--- openlane/computer/config.tcl
+++ openlane/computer/config.tcl
@@ -42,9 +42,11 @@ set ::env(CLOCK_PORT) wb_clk_i
 set ::env(CLOCK_NET) wb_clk_i
 set ::env(CLOCK_PERIOD) 10
 
-set ::env(PL_TARGET_DENSITY) 0.30
+set ::env(PL_TARGET_DENSITY) 0.1
 set ::env(FP_SIZING) absolute
 set ::env(DIE_AREA) "0 0 2000 2000"
+
+set ::env(GLB_RT_MAXLAYER) 5
 
 set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
 
